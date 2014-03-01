1d2212581b08f4d37c2953668a14438c12d023f3
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-03-01 17:45:56 -0600

    updated classes

--- Farouche/com.Farouche.Commons/ShippingOrderLineItem.cs
+++ Farouche/com.Farouche.Commons/ShippingOrderLineItem.cs
@@ -1,13 +1,115 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+//Author: Andrew
+//Date Created: 2/23/2014
+//Last Modified: 3/1/2014 
+//Last Modified By: Andrew Willhoit
+
+/*
+*                               Changelog
+* Date         By          Ticket          Version         Description
+* 
+* 
+*/
 
 namespace com.Farouche.Commons
 {
     class ShippingOrderLineItem
     {
+        private int _shippingOrderId;
+        private int _productID;
+        private String _productName;
+        private int _quantity;
+        private String _productLocation;
+        private Boolean _isPicked;
 
-        //comment
+        public ShippingOrderLineItem()
+        {
+        }
+
+        public ShippingOrderLineItem(int shippingOrderId, int productId)
+        {
+            this.ShippingOrderID = shippingOrderId;
+            this.ProductId = productId;
+        }
+
+        public int ShippingOrderID
+        {
+            get
+            {
+                return _shippingOrderId;
+            }
+            set
+            {
+                _shippingOrderId = value;
+            }
+        }
+        public int ProductId
+        {
+            get
+            {
+                return _productID;
+            }
+            set
+            {
+                _productID = value;
+            }
+        }
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+            set
+            {
+                _quantity = value;
+            }
+        }
+        public String ProductLocation
+        {
+            get
+            {
+                return _productLocation;
+            }
+            set
+            {
+                _productLocation = value;
+            }
+        }
+        public Boolean IsPicked
+        {
+            get
+            {
+                return _isPicked;
+            }
+            set
+            {
+                _isPicked = value;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("ShippingOrderID: {0}, ProductId: {1}, ProductName, {2}, Quantity: {3}, ProductLocation: {4}, IsPicked: {5}", ShippingOrderID, ProductId, ProductName, Quantity, ProductLocation, IsPicked);
+        }
+        public Type GetType()
+        {
+            throw new NotImplementedException();
+        }
+        public string ToXml()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
