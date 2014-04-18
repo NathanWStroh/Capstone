3893e15d218416b472e793d474e8cde398ab1a13
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-17 19:34:27 -0500

    change sp_DeactivateCategory to proc_

--- Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_DeactivateCategory.proc.sql
+++ Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_DeactivateCategory.proc.sql
@@ -1,6 +1,6 @@
 ﻿
-/*Object: StoredProcedure [dbo].[sp_DeactivateCategory] */
-CREATE PROCEDURE [sp_DeactivateCategory]
+/*Object: StoredProcedure [dbo].[proc_DeactivateCategory] */
+CREATE PROCEDURE [proc_DeactivateCategory]
 	(@category varchar(50))
 AS
 	UPDATE [dbo].[Categories]
