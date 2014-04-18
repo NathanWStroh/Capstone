6c2cd771980238693cce76bc0aada78fdd23c386
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-17 23:26:55 -0500

    change sp_UpdateVendorName to proc_

--- Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_UpdateVendorName.proc.sql
+++ Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_UpdateVendorName.proc.sql
@@ -1,7 +1,7 @@
 ﻿
 
-/*Object:  StoredProcedure [dbo].[sp_UpdateVendorName]*/
-CREATE PROCEDURE [dbo].[sp_UpdateVendorName]
+/*Object:  StoredProcedure [dbo].[proc_UpdateVendorName]*/
+CREATE PROCEDURE [dbo].[proc_UpdateVendorName]
 	(@Name varchar(50),
 	 @original_VendorID int,
 	 @original_Name varchar(50))
