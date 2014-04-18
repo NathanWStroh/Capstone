f598b412950919b899491cd203179e8cf2114a4c
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-17 23:05:03 -0500

    change sp_ReactivateCategory to proc_

--- Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_ReactivateCategory.proc.sql
+++ Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_ReactivateCategory.proc.sql
@@ -1,6 +1,6 @@
 ﻿
-/*Object: StoredProcedure [dbo].[sp_ReactivateCategory] */
-CREATE PROCEDURE [sp_ReactivateCategory]
+/*Object: StoredProcedure [dbo].[proc_ReactivateCategory] */
+CREATE PROCEDURE [proc_ReactivateCategory]
 	(@category varchar(50))
 AS
 	UPDATE [dbo].[Categories]
