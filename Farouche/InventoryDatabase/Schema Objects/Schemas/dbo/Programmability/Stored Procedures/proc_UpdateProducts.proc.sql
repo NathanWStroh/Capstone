012035ce93224354fdda69fb3180bd2d7501d8c0
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-17 23:22:59 -0500

    change sp_UpdateProducts to proc_

--- Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_UpdateProducts.proc.sql
+++ Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_UpdateProducts.proc.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dbo].[sp_UpdateProducts]
+﻿CREATE PROCEDURE [dbo].[proc_UpdateProducts]
 	(@ProductID						Int,
 	@Available						Int,
 	@OnHand							Int,
--- Farouche/com.Farouche.DataAccess/ProductDAL.cs
+++ Farouche/com.Farouche.DataAccess/ProductDAL.cs
@@ -87,7 +87,7 @@ namespace com.Farouche.DataAccess
                 //Establishes the connection.
                 conn.Open();
                 //Creates the command object, passing the SP and connection object.
-                SqlCommand sqlCmd = new SqlCommand("sp_UpdateProducts", conn);
+                SqlCommand sqlCmd = new SqlCommand("proc_UpdateProducts", conn);
                 sqlCmd.CommandType = CommandType.StoredProcedure;
                 sqlCmd.Parameters.AddWithValue("@ProductID", originalProduct.Id);
                 sqlCmd.Parameters.AddWithValue("@OnHand", product.reserved);
