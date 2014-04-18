5e96e153e6457c946278fa04d251fd96cc597cf2
Andrew Willhoit <andrew.willhoit@gmail.com>
2014-04-17 21:33:29 -0500

    change sp_DeleteLocation to proc_

--- Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_DeleteLocation.proc.sql
+++ Farouche/InventoryDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/proc_DeleteLocation.proc.sql
@@ -1,5 +1,5 @@
-﻿/*Object: StoredProcedure [dbo].[sp_DeleteLocation] */
-CREATE PROCEDURE [sp_DeleteLocation]
+﻿/*Object: StoredProcedure [dbo].[proc_DeleteLocation] */
+CREATE PROCEDURE [proc_DeleteLocation]
 	(@location varchar(250))
 AS
 	DELETE FROM [dbo].[Locations]
